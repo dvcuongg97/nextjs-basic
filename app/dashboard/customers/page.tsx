bcb7a12abddc97630888e174670f0eade6e31285
dvcuong97 <cuonggdv08@gmail.com>
2023-11-18 17:57:03 +0700

    push

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <p className="bg-lime-300">Customers Page</p>;
+};
+
+export default page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <p className="bg-rose-400">Invoices Page</p>;
+};
+
+export default page;
